7b7a1e2a692d992064a3b6b2c2629c9922b9d08a
Sarah Ohlin <sarahohlin1@gmail.com>
2025-08-08 16:52:26 -0400

    wip

--- init.lua
+++ init.lua
@@ -1,28 +1,62 @@
--- vim: foldlevelstart=0
+-- vim:foldlevel=99
 require('lazystrap')
 
 -- {{{ Plugins
 require('lazy').setup({
-	{"nvim-treesitter/nvim-treesitter", branch = 'master', lazy = false, build = ":TSUpdate"},
-	{"rebelot/kanagawa.nvim", branch = "master", lazy = false, priority = 1000,
+	{ "nvim-treesitter/nvim-treesitter", branch = 'master', lazy = false, build = ":TSUpdate" },
+	{ "rebelot/kanagawa.nvim", branch = "master", lazy = false, priority = 1000,
 		config = function()
 			vim.cmd([[colorscheme kanagawa]])
 		end
 	},
 	{ "nvim-tree/nvim-web-devicons", lazy = true },
+	{ "neovim/nvim-lspconfig" },
+	{ "onsails/lspkind.nvim" }
 })
+
+require('lsp')
 -- }}}
 
 -- {{{ Vim Options
+
+vim.opt.background = "dark"
+vim.opt.mouse = "a"
+vim.opt.signcolumn = "yes:1"
+vim.opt.incsearch = true
+vim.opt.hlsearch = true
+
+vim.opt.showmode = false
+vim.opt.fillchars = "eob:\\"
+
+vim.opt.re = 2
+vim.opt.equalalways = true
+vim.opt.completeopt = "menuone,noinsert,noselect"
+
+vim.cmd([[
+filetype plugin on
+filetype plugin indent on
+]])
+
+-- tabs
 vim.opt.tabstop = 4
 vim.opt.expandtab = false
 vim.opt.shiftwidth = 4
 vim.opt.softtabstop = 4
+vim.opt.smartindent = false
+
 vim.opt.modeline = true
 vim.opt.foldmethod = "marker"
 vim.opt.foldlevelstart = 0
+
+-- line numbers
+vim.opt.cindent = true
+vim.opt.cino = "g0N-sj1:0"
+
+vim.opt.termguicolors = true
+
 -- }}}
 
+-- {{{ Keybinds
 function noremap(mode, shortcut, command)
   vim.api.nvim_set_keymap(mode, shortcut, command, { noremap = true })
 end
@@ -45,3 +79,4 @@ end
 
 nmap('<leader>p', '"+p')
 nmap('<leader>y', '"+y')
+-- }}}
